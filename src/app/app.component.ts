8c31e64369fd15300e1646766376454bdd9d6dd2
Martin Askestad <MartinAskestad@users.noreply.github.com>
2018-12-01 18:56:21 +0100

    Fixed bug with redraw of page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import * as pack from 'bin-pack';
-import { BehaviorSubject } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -29,32 +29,24 @@ export class AppComponent {
     'teal',
     'aqua'
   ];
-  packedResult: any;
   form: FormGroup;
   viewBox$ = this.viewBoxSubject.asObservable();
-
-  get viewBox(): string {
-    if (this.packedResult) {
-      return `0 0 ${this.packedResult.width} ${this.packedResult.height}`;
-    }
-    return '0 0 100 100';
-  }
+  packedResult$: Observable<any>;
 
   constructor() {
     this.form = new FormGroup({
       bins: new FormArray([])
     });
 
-    this.form.valueChanges
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged()
-      )
-      .subscribe(values => {
-        const size = pack(values.bins, { inPlace: true });
-        this.viewBoxSubject.next(`0 0 ${size.width} ${size.height}`);
-        this.form.patchValue(values, { emitEvent: false });
-      });
+    this.packedResult$ = this.form.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      map(values => {
+        const result = pack(values.bins);
+        this.viewBoxSubject.next(`0 0 ${result.width} ${result.height}`);
+        return result.items;
+      })
+    );
   }
 
   addBin() {
